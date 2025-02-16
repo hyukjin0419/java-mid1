3cbe7a5c3405d72f718245d0fffe2cb74bd92469
hyukjin0419 <21900771@handong.ac.kr>
2025-02-16 10:14:28 +0900

    2025.02.16 start

--- src/baekjoon/silver/설탕_배달_2839.java
+++ src/baekjoon/silver/설탕_배달_2839.java
@@ -6,19 +6,23 @@ public class 설탕_배달_2839 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int num = Integer.parseInt(br.readLine());
-        int sum = 0;
+        int input = Integer.parseInt(br.readLine());
+        int result = 0;
 
-        while (num >= 0) {
-            if (num % 5 == 0) {
-                sum += num / 5;
-                System.out.println(sum);
-                return;
-            } else {
-                num -= 3;
-                sum ++;
+        while (input >= 1) {
+            if (input % 5 == 0) {
+                result += input/5;
+                break;
+            } else{
+                input -=3;
+                result++;
             }
         }
-        System.out.println(-1);
+
+        if (input < 0) {
+            System.out.println(-1);
+        }else {
+            System.out.println(result);
+        }
     }
 }
