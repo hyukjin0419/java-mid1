ba5ca7435ab1645c63a72a7e67146711f1592fe3
hyukjin0419 <21900771@handong.ac.kr>
2025-02-19 22:23:09 +0900

    2025.02.19 1

--- src/baekjoon/algorithm/sort/SortAlgorithmBasic.java
+++ src/baekjoon/algorithm/sort/SortAlgorithmBasic.java
@@ -5,7 +5,6 @@ public class SortAlgorithmBasic {
 
     static void bubbleSort(int[] arr){
         int n = arr.length;
-
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
