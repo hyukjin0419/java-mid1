3a65dcd64e6986b354d09620acbb31166c3bfc0f
hyukjin0419 <21900771@handong.ac.kr>
2025-03-05 22:10:55 +0900

    2025.03.05 2

--- src/baekjoon/약수_배수와_소수/최소공배수_1934.java
+++ src/baekjoon/약수_배수와_소수/최소공배수_1934.java
@@ -4,15 +4,9 @@ import java.io.*;
 import java.util.*;
 
 public class 최소공배수_1934 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        /*라
-        A의 배수이면서
-        B의 배수인 수 중 가장 작은 수를 구하여라
-
-         */
 
         int n = Integer.parseInt(br.readLine());
 
@@ -23,20 +17,33 @@ public class 최소공배수_1934 {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            int gcd = getGcd(a, b);
-            int lcm = a * b / gcd;
-            bw.write(lcm + "\n");
+
+            bw.write(lcm(a,b,gcd(a,b)) + "\n");
         }
 
         bw.flush();
+
     }
 
-    private static int getGcd(int a, int b) {
-        while (b != 0) {
-            int temp = a%b;
-            a = b;
-            b = temp;
+    static int gcd(int a, int b) {
+        if(a<b){
+            int temp = b;
+            b = a;
+            a = temp;
         }
-        return a;
+
+        int gcd = 0;
+
+        for (gcd = b; gcd > 0; gcd--) {
+            if (a % gcd == 0 && b % gcd == 0) {
+                return gcd;
+            }
+        }
+
+        return -1;
+    }
+
+    static int lcm(int a, int b, int g) {
+        return a * b / g;
     }
 }
