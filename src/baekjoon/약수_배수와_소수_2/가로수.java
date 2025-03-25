5af0cba1b8a7f82dea9df3e880bcbd5fd7fa48ee
hyukjin0419 <21900771@handong.ac.kr>
2025-03-25 22:56:32 +0900

    2025.03.24 1

--- src/baekjoon/약수_배수와_소수_2/가로수.java
+++ src/baekjoon/약수_배수와_소수_2/가로수.java
@@ -6,42 +6,35 @@ import java.io.*;
 public class 가로수 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        /*
-        입력을 받으면서 바로 gcd를 구해서
-        Min value update!
-        이때 처음과 마지막 수는 저장해놓는다.
-
-        이후 처음에서 minValueGcd만큼 증가하면서 출력한다. end에 도달할 때까지
-         */
         int rep = Integer.parseInt(br.readLine());
-        long start;
-        long n1,n2;
-        long end = 0;
-        long min = 0;
+        int[] trees = new int[rep];
 
-        start = Integer.parseInt(br.readLine());
-        n1 = start;
-        for(int i=1;i<rep;i++){
-            n2 = Integer.parseInt(br.readLine());
-            long gcd = gcd(n1,n2);
-            if(gcd < min) min = gcd;
-
-            if(i==rep-1){
-                end = n2;
-            }
-
-            n1 = n2;
+        for (int i = 0; i < rep; i++) {
+            trees[i] = Integer.parseInt(br.readLine());
         }
 
-        long result = ((end-start)/min) + 1 -rep;
+        int[] gaps = new int[rep - 1];
+        for (int i = 0; i < gaps.length; i++) {
+            gaps[i] = trees[i + 1] - trees[i];
+        }
+
+        int gcdValue = gaps[0];
+        for (int i = 1; i < gaps.length; i++) {
+            gcdValue = gcd(gcdValue, gaps[i]);
+        }
+
+        int result = 0;
+        for (int gap : gaps) {
+            result += (gap / gcdValue) - 1;
+        }
+
         System.out.println(result);
     }
 
-    static long gcd(long a,long b){
+    static int gcd(int a,int b){
         while(b != 0){
-            long temp = b;
+            int temp = b;
             b = a % b;
             a = temp;
         }
